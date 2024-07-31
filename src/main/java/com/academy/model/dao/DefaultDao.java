74c39ff1ad635ee0f4792cd8b929ce20405f70d8
ihar vypau <igor.vyyy@yandex.ru>
2024-07-31 10:55:57 +0300

    edit return type in findById method

--- src/main/java/com/academy/model/dao/DefaultDao.java
+++ src/main/java/com/academy/model/dao/DefaultDao.java
@@ -3,5 +3,5 @@ package com.academy.model.dao;
 public interface DefaultDao<T> {
     void createOrUpdate(T entity);
     void delete(T entity);
-    <T> T findById(Integer id);
+    T findById(Integer id);
 }
